04423f8beb8b1ec5c2035e5ece8e0b067f588f24
Prayag Gordy <prayaggordy@gmail.com>
2021-07-15 00:42:49 -0400

    create db

--- main.R
+++ main.R
@@ -4,4 +4,7 @@ sapply(list.files(path = "R", full.names = T), source, .GlobalEnv)
 
 covid <- download_nyt()
 census <- download_census()
+pres <- readr::read_csv(paste0(config$paths$raw, config$data$pres))
+vaccination <- readr::read_csv(paste0(config$paths$us_ts, config$data$vax_data_ts))
 
+create_sql(update = T)
