ce21e71d08df512f992d460c5c66af4a98139130
Prayag Gordy <prayaggordy@gmail.com>
2021-07-15 00:44:06 -0400

    remove update=T

--- main.R
+++ main.R
@@ -7,4 +7,4 @@ census <- download_census()
 pres <- readr::read_csv(paste0(config$paths$raw, config$data$pres))
 vaccination <- readr::read_csv(paste0(config$paths$us_ts, config$data$vax_data_ts))
 
-create_sql(update = T)
+create_sql()
