32921366e1c1bca595208afeec0b80b93f72b29a
Prayag Gordy <prayaggordy@gmail.com>
2021-07-19 17:49:40 -0400

    wrote into a function, used pivot_longer and across to clean up code

--- R/dm_census.R
+++ R/dm_census.R
@@ -2,19 +2,37 @@ library(tidycensus)
 library(tidyverse)
 library(magrittr)
 
-acs19 <- get_acs(geography = "county",
-								 variables = c(med_income = "B19013_001", male = "B01001_002", female = "B01001_026",
-								 							 med_age = "B01002_001", white = "B01001A_001", black = "B01001B_001",
-								 							 hispanic = "B01001I_001", asian = "B01001D_001", total = "B01001_001",
-								 							 other = "B01001F_001"),
-								 year = 2019,
-								 moe_level = 95)
-acs19_subset <- acs19[, c("GEOID","NAME", "variable","estimate")]
-acs19_spread <- spread(acs19_subset, key = variable, value = estimate)
+download_census <- function(fn,
+														path_raw,
+														path_proc,
+														update = F) {
 
-county_data <- mutate(acs19_spread, percent_asian=asian/total, percent_white=white/total,
-											percent_black=black/total, percent_hispanic=hispanic/total,
-											percent_male=male/total, percent_female=female/total,
-											percent_other=other/total)
+	if (file.exists(fn_proc) & !update)
+		return(readr::read_csv(fn_proc))
 
-county_data <- county_data[,c(1,2,12,4,3,6,10,7,5,11,14,15,13,16,19,17,18,8,9)]
+	fn_raw <- paste0(path_raw, fn)
+	fn_proc <- paste0(path_proc, fn)
+
+	if (!file.exists(fn_raw) | update) {
+		df <- get_acs(geography = "county",
+									variables = c(med_income = "B19013_001", male = "B01001_002", female = "B01001_026",
+																med_age = "B01002_001", white = "B01001A_001", black = "B01001B_001",
+																hispanic = "B01001I_001", asian = "B01001D_001", total = "B01001_001",
+																other = "B01001F_001"),
+									year = 2019,
+									moe_level = 95)
+		readr::write_csv(df, fn_raw)
+	} else {
+		df <- readr::read_csv(fn_raw)
+	}
+
+	df <- df %>%
+		dplyr::select(-moe) %>%
+		dplyr::rename(fips = GEOID, county = NAME) %>%
+		tidyr::pivot_wider(names_from = variable, values_from = estimate) %>%
+		dplyr::mutate(dplyr::across(.cols = male:hispanic, .fns = ~ ./total, .names = "percent_{.col}"))
+
+	readr::write_csv(df, fn_proc)
+
+	df
+}
