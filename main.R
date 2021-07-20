a0a5731586db3077b21013c956b6a1c1e180a6e2
Ankit Patel <ankitpatel@Ankits-MBP.fios-router.home>
2021-07-20 10:47:07 -0400

    load libraries for dm_census and main

--- main.R
+++ main.R
@@ -1,10 +1,10 @@
-library(magrittr); library(RSQLite)
-config <- yaml::read_yaml("config.yaml")
+library(magrittr); library(RSQLite); library(yaml); library(readr)
+config <- read_yaml("config.yaml")
 sapply(list.files(path = "R", full.names = T), source, .GlobalEnv)
 
 covid <- download_nyt()
 census <- download_census()
-pres <- readr::read_csv(paste0(config$paths$raw, config$data$pres))
-vaccination <- readr::read_csv(paste0(config$paths$us_ts, config$data$vax_data_ts))
+pres <- read_csv(paste0(config$paths$raw, config$data$pres))
+vaccination <- read_csv(paste0(config$paths$us_ts, config$data$vax_data_ts))
 
 create_sql()
