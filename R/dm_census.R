a1d2d2b23df315ec2bba49fd5de5afbad6c4667a
Ankit Patel <ankitpatel@Ankits-MacBook-Pro.local>
2021-07-24 21:29:05 -0700

    my attempts at faceting by region, changing name of dm_vaccination and moving it into R folder

--- R/dm_census.R
+++ R/dm_census.R
@@ -13,7 +13,7 @@ download_census <- function(fn = config$data$census,
 		return(read_csv(fn_proc))
 
 	if (!file.exists(fn_raw) | update) {
-		df <- get_acs(geography = "county",
+		df <- get_acs(geography = c("region"),
 									variables = c(med_income = "B19013_001", male = "B01001_002",
 																female = "B01001_026", med_age = "B01002_001",
 																white = "B01001A_001", black = "B01001B_001",
