920b3a58e8308a7beeb95abd3a99d232d706bdd9
Hallie Trial <hot2@rice.edu>
2021-07-24 19:07:22 -0500

    Robust regression

--- R/plot_presvacc.R
+++ R/plot_presvacc.R
@@ -1,15 +1,17 @@
 library(ggplot2); library(RSQLite); library(yaml); library(MASS)
 config <- read_yaml("config.yaml")
 
-presvacc <- filter(get_vacc_pres_df(2020.0), !is.na(FIPS))
+presvacc <- filter(get_vacc_pres_df(2020.0), !is.na(FIPS) & !is.na(pct_vacc) & !is.na(trump_pct))
 
 
 rr.huber <- rlm(pct_vacc~trump_pct, data = presvacc)
-resid_by_county <- data.frame(County = presvacc$County.y, resid = rr.huber$resid)
+
+inter <- rr.huber$coefficients[1]
+slope <- rr.huber$coefficients[2]
+
+resid_by_county <- data.frame(County = presvacc$County.y, FIPS = presvacc$FIPS, resid = rr.huber$resid)
 head(resid_by_county)
 
-what <- filter(presvacc, pct_vacc <0.1)
-what
 
 ggplot(presvacc)+aes(x=trump_pct, y = pct_vacc)+
 	ggtitle("Proportion Vaccinated Versus Proportion Voting for Trump by County")+
@@ -18,7 +20,7 @@ ggplot(presvacc)+aes(x=trump_pct, y = pct_vacc)+
 	scale_color_manual(values = c("blue","red"))+
 	geom_rect(mapping=aes(xmin=0, xmax=0.5, ymin=-0.01, ymax=1.01, fill=I('lightskyblue2')), color = 'lightskyblue2')+
 	geom_rect(mapping=aes(xmin=0.5, xmax=1.0, ymin=-0.01, ymax=1.01, fill=I('lightsalmon2')), color = 'lightsalmon2')+
-
+	geom_abline(intercept = inter, slope = slope)+
 	geom_point(alpha = 1/2)
 
 
