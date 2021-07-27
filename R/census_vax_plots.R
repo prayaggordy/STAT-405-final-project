b95b0aa37a7986adc621829fea6bd89924dfd29c
Prayag Gordy <prayaggordy@gmail.com>
2021-07-27 02:24:36 -0400

    add fill

--- R/census_vax_plots.R
+++ R/census_vax_plots.R
@@ -9,12 +9,13 @@ plot_vax_by_region <- function(df = vaccination,
 		inner_join(xwalk, by = "state_fips") %>%
 		mutate(region = str_remove(region, " Region"))
 
-	ggplot(df, aes(x = fully_vax)) +
+	ggplot(df, aes(x = fully_vax, fill = region)) +
 		geom_histogram(binwidth = 0.1) +
 		facet_wrap(~region, scales = "free_y") +
 		theme_minimal() +
 		labs(x = "Percent of adults fully vaccinated",
 				 y = "Number of counties",
 				 title = "Percent vaccinated by county") +
-		scale_x_continuous(labels = scales::percent)
+		scale_x_continuous(labels = scales::percent) +
+		theme(legend.position = "none")
 }
