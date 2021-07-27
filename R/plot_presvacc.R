4f245503899b05b58ecc17441a480de683d4cd69
Prayag Gordy <prayaggordy@gmail.com>
2021-07-27 00:52:27 -0400

    single function to make trump plot

--- R/plot_presvacc.R
+++ R/plot_presvacc.R
@@ -3,7 +3,26 @@ config <- read_yaml("config.yaml")
 
 plot_trump_vacc <- function(df_pres = pres,
 														df_vacc = vaccination) {
-
+	df_vacc %>%
+		dplyr::filter(date == max(date)) %>%
+		dplyr::inner_join(df_pres, by = "fips") %>%
+		ggplot(aes(x = percent_trump, y = fully_vax)) +
+		geom_rect(mapping = aes(xmin = 0, xmax = 0.5,
+														ymin = -0.01, ymax = 1.01,
+														fill = I('lightskyblue2')),
+							color = 'lightskyblue2') +
+		geom_rect(mapping = aes(xmin = 0.5, xmax = 1.0,
+														ymin = -0.01, ymax = 1.01,
+														fill = I('lightsalmon2')),
+							color = 'lightsalmon2') +
+		geom_point(alpha = 1/3) +
+		geom_smooth(method = "lm", se = F, color = "black") +
+		labs(x = "Proportion of Votes Cast for Trump",
+				 y = "Proportion of Population Vaccinated",
+				 title = "Proportion Vaccinated Versus Proportion Voting for Trump by County") +
+		theme_minimal() +
+		scale_x_continuous(labels = scales::percent) +
+		scale_y_continuous(labels = scales::percent)
 }
 
 #blue_demo <- inner_join(filter(presvacc, trump_pct < 0.4), demos)
@@ -46,42 +65,27 @@ make_race_hist_plots <- function(){
 	grid.arrange(p1, p2, p3, p4, nrow=2)
 }
 
-
-
-plot_trump_vacc <- function(){
-	ggplot(presvacc)+aes(x=trump_pct, y = pct_vacc)+
-		ggtitle("Proportion Vaccinated Versus Proportion Voting for Trump by County")+
-		ylab("Proportion of Population Vaccinated") +
-		xlab("Proportion of Votes Cast for Trump")+
-		theme_minimal()+
-		geom_rect(mapping=aes(xmin=0, xmax=0.5, ymin=-0.01, ymax=1.01, fill=I('lightskyblue2')), color = 'lightskyblue2')+
-		geom_rect(mapping=aes(xmin=0.5, xmax=1.0, ymin=-0.01, ymax=1.01, fill=I('lightsalmon2')), color = 'lightsalmon2')+
-		theme(axis.text = element_text(size = 8), axis.title = element_text(size = 10), title = element_text(size = 12))+
-		geom_abline(intercept = inter, slope = slope)+
-		geom_point(alpha = 1/3)
-}
-
-
-presvacc <- filter(get_vacc_pres_df(2020.0), !is.na(FIPS) & !is.na(pct_vacc) & !is.na(trump_pct))
-demos <- get_census_data()
-presvacc_2016 <- filter(get_vacc_pres_df(2016.0), !is.na(FIPS) & !is.na(pct_vacc) & !is.na(trump_pct))
-
-resid_by_county <- data.frame(County = presvacc$County.y, FIPS = presvacc$FIPS, trump_pct = presvacc$trump_pct, resid = rr.huber$resid)
-resid_sorted <- resid_by_county[order(resid_by_county$resid),]
-rows <- nrow(resid_sorted)
-ten_percent <- round(rows*0.1)
-bottom_resid <- resid_sorted[1:ten_percent,]
-top_resid <- resid_sorted[(rows-ten_percent):rows,]
-
-bottom <- inner_join(bottom_resid, demos, by='FIPS')
-top <- inner_join(top_resid, demos, by = 'FIPS')
-
-
-bottom_and_blue <- filter(bottom, trump_pct < 0.4)
-bottom_and_red <- filter(bottom, trump_pct >0.6)
-
-top_and_red <- filter(top, trump_pct > 0.6)
-top_and_blue <- filter(top, trump_pct < 0.4)
-
-plot_trump_vacc()
-make_race_hist_plots()
+#
+# presvacc <- filter(get_vacc_pres_df(2020.0), !is.na(FIPS) & !is.na(pct_vacc) & !is.na(trump_pct))
+# demos <- get_census_data()
+# presvacc_2016 <- filter(get_vacc_pres_df(2016.0), !is.na(FIPS) & !is.na(pct_vacc) & !is.na(trump_pct))
+#
+# resid_by_county <- data.frame(County = presvacc$County.y, FIPS = presvacc$FIPS, trump_pct = presvacc$trump_pct, resid = rr.huber$resid)
+# resid_sorted <- resid_by_county[order(resid_by_county$resid),]
+# rows <- nrow(resid_sorted)
+# ten_percent <- round(rows*0.1)
+# bottom_resid <- resid_sorted[1:ten_percent,]
+# top_resid <- resid_sorted[(rows-ten_percent):rows,]
+#
+# bottom <- inner_join(bottom_resid, demos, by='FIPS')
+# top <- inner_join(top_resid, demos, by = 'FIPS')
+#
+#
+# bottom_and_blue <- filter(bottom, trump_pct < 0.4)
+# bottom_and_red <- filter(bottom, trump_pct >0.6)
+#
+# top_and_red <- filter(top, trump_pct > 0.6)
+# top_and_blue <- filter(top, trump_pct < 0.4)
+#
+# plot_trump_vacc()
+# make_race_hist_plots()
