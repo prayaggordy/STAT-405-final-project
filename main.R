c20c8271c8315e0facc38f586577b37647301c84
Prayag Gordy <prayaggordy@gmail.com>
2021-07-27 00:37:27 -0400

    data management on pres data

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ sapply(list.files(path = "R", full.names = T), source, .GlobalEnv)
 
 covid <- download_nyt()
 census <- download_census()
-pres <- read_csv(paste0(config$paths$raw, config$data$pres))
+pres <- dm_pres()
 tx_vaccination <- read_csv(paste0(config$paths$raw, config$data$tex_vacc))
 small_ca_vacc <- read_csv(paste0(config$paths$raw, config$data$small_ca_vacc))
 vaccination <- download_vacc()
