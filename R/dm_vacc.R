88b2a715d97465e9096444e4b12bf868c9d1cf43
Prayag Gordy <prayaggordy@gmail.com>
2021-07-27 02:09:10 -0400

    download and process texas data

--- R/dm_vacc.R
+++ R/dm_vacc.R
@@ -11,10 +11,10 @@ library(magrittr)
 config <- yaml::read_yaml("config.yaml")
 
 download_vacc <- function(u = "https://data.cdc.gov/api/views/8xkx-amqh/rows.csv?accessType=DOWNLOAD",
-												 fn = config$data$cdc_vacc,
-												 path_raw = config$paths$raw,
-												 path_proc = config$paths$proc,
-												 update = F) {
+													fn = config$data$cdc_vacc,
+													path_raw = config$paths$raw,
+													path_proc = config$paths$proc,
+													update = F) {
 
 	fn_proc <- paste0(path_proc, fn)
 	if (file.exists(fn_proc) & !update) {
@@ -36,6 +36,57 @@ download_vacc <- function(u = "https://data.cdc.gov/api/views/8xkx-amqh/rows.csv
 					 across(c(first_dose, fully_vax), ~as.numeric(.)/100))
 
 	write_csv(df, fn_proc)
+
+	df
+}
+
+download_tx_vacc <- function(u, fn, path_raw, path_proc, sheet, update) {
+
+	fn_raw <- paste0(path_raw, fn)
+	fn_proc <- paste0(path_proc, fn)
+	if (file.exists(fn_proc) & !update) {
+		return(readr::read_csv(fn_proc))
+	}
+
+	if (file.exists(fn_raw) & !update) {
+		df <- readr::read_csv(fn_raw)
+	} else {
+		df <- openxlsx::read.xlsx(u, sheet = sheet)
+		readr::write_csv(df, fn_raw)
+	}
+
+	df <- df %>%
+		janitor::clean_names() %>%
+		dplyr::slice(-c(1:3))
+
+	df
+}
+
+dm_texas_vacc <- function(u = "https://www.dshs.texas.gov/immunize/covid19/COVID-19-Vaccine-Data-by-County.xls",
+													fn = config$data$tex_vacc,
+													path_raw = config$paths$raw,
+													path_proc = config$paths$proc,
+													sheet = 2,
+													update = F,
+													xwalk = xwalk_fips,
+													census = census_county) {
+
+	df <- download_tx_vacc(u = u,
+												 fn = fn,
+												 path_raw = path_raw,
+												 path_proc = path_proc,
+												 sheet = sheet,
+												 update = update) %>%
+		dplyr::mutate(county_name = tolower(county_name)) %>%
+		dplyr::inner_join(xwalk %>%
+												dplyr::filter(state_name == "Texas"),
+											by = c("county_name" = "county_short_lc")) %>%
+		dplyr::select(fips,
+									first_dose = people_vaccinated_with_at_least_one_dose,
+									fully_vax = people_fully_vaccinated) %>%
+		dplyr::inner_join(census, by = "fips") %>%
+		dplyr::mutate(dplyr::across(c(first_dose, fully_vax), ~ as.numeric(.) / total)) %>%
+		dplyr::select(fips, first_dose, fully_vax)
 
 	df
 }
