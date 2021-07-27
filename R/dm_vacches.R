51b184a51d8f0d4f97da172f2f2322a8841aba57
Prayag Gordy <prayaggordy@gmail.com>
2021-07-26 23:34:23 -0400

    add minimal cleaning steps

--- R/dm_vacches.R
+++ R/dm_vacches.R
@@ -11,16 +11,28 @@ library(magrittr)
 config <- yaml::read_yaml("config.yaml")
 
 download_vacches <- function(u = "https://data.cdc.gov/api/views/q9mh-h2tw/rows.csv?accessType=DOWNLOAD",
-													fn = config$data$vacc_hes,
-													path_raw = config$paths$raw,
-													path_proc = config$paths$proc,
-													update = F) {
+														 fn = config$data$vacc_hes,
+														 path_raw = config$paths$raw,
+														 path_proc = config$paths$proc,
+														 update = F) {
+
+	fn_proc <- paste0(path_proc, fn)
+	if (file.exists(fn_proc) & !update) {
+		return(read_csv(fn_proc))
+	}
 
 	df <- download_data(u = u,
 											fn_full = paste0(path_raw, fn),
 											update = update)
 
-	# this is where we do any data processing
+	df <- df %>%
+		janitor::clean_names() %>%
+		dplyr::select(fips = fips_code,
+									svi = social_vulnerability_index_svi,
+									svi_category) %>%
+		dplyr::mutate(fips = stringr::str_pad(fips, width = 5, side = "left", pad = "0"))
+
+	write_csv(df, fn_proc)
 
 	df
 }
