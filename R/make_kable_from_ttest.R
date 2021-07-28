25b7e4dcabef8cc6457b9f70e0ab82ebc92e4f96
Hallie Trial <hot2@rice.edu>
2021-07-28 11:24:02 -0500

    Made kable from my t test

---
+++ R/make_kable_from_ttest.R
@@ -0,0 +1,16 @@
+library(knitr)
+library(kableExtra)
+library(purrr)
+library(broom)
+library(dplyr)
+library(magrittr)
+
+get_kable <- function(ttest){
+	map_df(list(ttest), tidy) %>% select(c(estimate, estimate1, estimate2, p.value, conf.low, conf.high, alternative)) %>%
+		kable(col.names = c("Difference in Means", "Mean, Vulnerable", "Mean, Not Vulnerable", "p value",
+												"95% Confidence Lower Bound", "95% Confidence Upper Bound", "Alternative Hypothesis")) -> tab
+	tab
+}
+
+
+
