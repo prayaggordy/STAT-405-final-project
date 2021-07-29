58edf411d9c90b93effa208fae1adc5b1ecb4fc3
Hallie Trial <hot2@rice.edu>
2021-07-29 01:11:26 -0500

    svi violinplots function

--- R/svi_plot.R
+++ R/svi_plot.R
@@ -18,6 +18,43 @@ make_df_for_sviplot <- function(df_hes = vaccine_hesitancy,
 	svi_df
 }
 
+
+get_df_svi_race <- function(df_svi = make_df_for_sviplot(), svi_upper, svi_lower){
+	db <- paste0(config$paths$proc, config$data$db)
+	dcon <- dbConnect(SQLite(), dbname = db)
+	query <- "
+	SELECT fips, percent_black
+	FROM census_county;"
+	res<- dbSendQuery(conn=dcon, query)
+	cens <- dbFetch(res, -1)
+	dbClearResult(res)
+	df_svi %>%
+		dplyr::filter((svi >= svi_lower) & (svi<=svi_upper)) %>%
+		inner_join(cens, by = "fips") -> svi_df
+	svi_df
+}
+
+make_svi_race_violinplot <- function(svi_upper, svi_lower){
+	df = get_df_svi_race(svi_upper = svi_upper, svi_lower=svi_lower)
+	vaccine_tag <- function(num){
+		if (num < 0.15){
+			returnval <- "Low vaccination"
+		}
+		else{
+			returnval <- "Moderate to high vaccination"
+		}
+		returnval
+	}
+	new_df <- mutate(df, vacc_tag =sapply(fully_vax, vaccine_tag))
+	ggplot(new_df, aes(x=vacc_tag, y=percent_black, fill = vacc_tag)) +
+		theme_minimal()+
+		labs(x = "Vaccination Level",
+				 y = "Percent Black",
+				 title = "Percent Black for Low Vaccination versus High Vaccination Vulnerable Counties",
+				 fill = "Vaccination Level")+
+		geom_violin()
+}
+
 get_vuln_not_vuln <- function() {
 
 	df <- make_df_for_sviplot()
