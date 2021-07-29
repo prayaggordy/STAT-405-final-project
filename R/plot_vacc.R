df7081fbf738e6bf1908fbaece55287554f6476e
Prayag Gordy <prayaggordy@gmail.com>
2021-07-29 11:37:10 -0400

    filter properly

--- R/plot_vacc.R
+++ R/plot_vacc.R
@@ -9,17 +9,19 @@ plot_deaths_pc_vaccine <- function(df_covid = covid,
 							 by = "fips") %>%
 		dplyr::mutate(deaths_pc = deaths/total*100000) %>%
 		dplyr::inner_join(df_vax %>%
-												dplyr::filter(date == max(date)),
+												dplyr::group_by(fips) %>%
+												dplyr::filter(date == max(date)) %>%
+												dplyr::ungroup(),
 											by = "fips")
 
 	ggplot(df, aes(x = deaths_pc, y = fully_vax)) +
-		geom_point(alpha = 0.75) +
+		geom_point(alpha = 0.5) +
 		geom_smooth(se = F, color = "cadetblue4", size = 2) +
 		labs(x = "Deaths per 100,000 residents",
 				 y = "Percent fully vaccinated",
 				 title = "Even counties with large numbers of deaths remain under-vaccinated") +
 		theme_minimal() +
-		scale_y_continuous(labels = scales::percent) +
+		scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
 		theme(plot.title.position = "plot")
 }
 
