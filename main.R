b37856c6dde6c758b6b7dcc6390609dfe6bf4adb
Prayag Gordy <prayaggordy@gmail.com>
2021-07-30 11:58:38 -0400

    don't load yaml

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
-library(magrittr); library(RSQLite); library(yaml); library(readr)
-config <- read_yaml("config.yaml")
+library(magrittr); library(RSQLite); library(readr)
+config <- yaml::read_yaml("config.yaml")
 sapply(list.files(path = "R", full.names = T), source, .GlobalEnv)
 
 xwalk_region <- dm_xwalk_regions()
